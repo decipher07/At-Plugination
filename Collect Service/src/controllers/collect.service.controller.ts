261fd04e436a2d2c120bda31461aea098305501a
decipher07 <social.deej@gmail.com>
2022-11-14 22:06:27 +0530

    ✨ feat: add delete operation

--- Collect Service/src/controllers/collect.service.controller.ts
+++ Collect Service/src/controllers/collect.service.controller.ts
@@ -4,7 +4,6 @@ import client from "../database/connect";
 import { QueryResult } from "pg";
 
 const createEntryInCollect = async ( req: Request, res: Response, next: NextFunction ) : Promise <Response> =>  {
-
     const { name, year_of_joining, phone, email, user_id } = req.body;
 
     const query = "INSERT INTO forms (name, year_of_joining, phone, email, user_id) VALUES($1,$2,$3,$4,$5) RETURNING *";
@@ -22,7 +21,6 @@ const createEntryInCollect = async ( req: Request, res: Response, next: NextFunc
 }
 
 const readEntryInCollect = async ( req: Request, res: Response, next: NextFunction ) : Promise <Response> => {
-
     const response_id : string = req.params.response_id;
 
     const query = "SELECT * FROM forms WHERE response_id=$1";
@@ -54,4 +52,21 @@ const updateEntryInCollect = async ( req: Request, res: Response, next: NextFunc
     }
 }
 
-export { createEntryInCollect, readEntryInCollect, updateEntryInCollect };
+/** Current Situation: Keeping updates to name for the data */
+const deleteEntryInCollect = async ( req: Request, res: Response, next: NextFunction ) : Promise <Response> => {
+    const response_id : number = req.body.response_id;
+
+    const query = "DELETE FROM forms WHERE response_id=$1";
+    const values = [ response_id ];
+
+    try {
+        let readQueryExecutionResult: QueryResult = await client.query(query, values);
+        return res.status(200).json({"success": true, "data": (readQueryExecutionResult.rowCount > 1 ? "Successful": "No Entry Exist"), "message": null});
+    } catch ( err : any ){
+        Logging.error(err.message);
+        return res.status(404).json({"success": false, "data": null, "message": "Error in reading data from database"});
+    }
+}
+
+
+export { createEntryInCollect, readEntryInCollect, updateEntryInCollect, deleteEntryInCollect };
--- Collect Service/src/routers/collect.service.routes.ts
+++ Collect Service/src/routers/collect.service.routes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import { createEntryInCollect, readEntryInCollect, updateEntryInCollect } from "../controllers/collect.service.controller"
+import { createEntryInCollect, deleteEntryInCollect, readEntryInCollect, updateEntryInCollect } from "../controllers/collect.service.controller"
 
 const router : Router = express.Router();
 
 router.post('/create', createEntryInCollect);
 router.get('/read/:response_id', readEntryInCollect);
 router.patch('/update', updateEntryInCollect);
+router.delete('/delete', deleteEntryInCollect);
 
 export = router;
