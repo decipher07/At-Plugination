993e86d618c48fa949a7dd0300d7629335c3fe10
decipher07 <social.deej@gmail.com>
2022-11-16 10:47:08 +0530

    🔐 feat: producing message to the queue

--- Sheets Service/src/config/config.ts
+++ Sheets Service/src/config/config.ts
@@ -7,6 +7,9 @@ const GOOGLE_CLIENT_ID : string = process.env.GOOGLE_CLIENT_ID || '';
 const GOOGLE_CLIENT_SECRET : string = process.env.GOOGLE_CLIENT_SECRET || '';
 const GOOGLE_REDIRECT_URI : string = process.env.GOOGLE_REDIRECT_URI || "http://localhost:3001/auth/redirect"
 
+const EXCHANGE_NAME : string = process.env.EXCHANGE_NAME || 'sheetsExchange';
+const QUEUE_NAME : string = process.env.EXCHANGE_NAME || 'sheetsQueue';
+
 export const config = {
     server: {
         port: SERVER_PORT
@@ -15,5 +18,9 @@ export const config = {
         clientId: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
         redirectUri: GOOGLE_REDIRECT_URI
+    },
+    queue: {
+        exchangeName: EXCHANGE_NAME,
+        queueName: QUEUE_NAME
     }
 };
--- Sheets Service/src/controllers/sheet.service.controller.ts
+++ Sheets Service/src/controllers/sheet.service.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { oauth2Client } from '../auth/utils';
 import { google } from 'googleapis';
+import { config } from '../config/config'
 import Logging from "../Library/logging";
+import sheetProducer from "../common/producerSheet.queue";
 
 const createSpreadSheetController = async (req: Request, res: Response) => {
     const title = req.body.title as string;
@@ -44,41 +46,48 @@ const appendSpreadSheetController = async (req: Request, res: Response) => {
     const { responseid, name, yearOfJoining, phone, email, userId } = req.body;
     const spreadsheetId = req.body.spreadsheetId;
 
-    try {
-        
-        oauth2Client.setCredentials({
-            refresh_token: refresh_token
-        })
+    /**
+     try {
+         
+         oauth2Client.setCredentials({
+             refresh_token: refresh_token
+         })
+     
+         oauth2Client.refreshAccessToken((err, tokens: any) => {
+             if (err) return console.error(err)
+     
+             oauth2Client.setCredentials({
+                 access_token: tokens.access_token
+             })
+         });
+     
+         const doc = google.sheets({ version: "v4", auth: oauth2Client });
+         
+         const resource = {
+             values: [
+                 [ responseid, name, yearOfJoining, phone, email, userId ]
+             ]
+         }
+     
+         let data = await doc.spreadsheets.values.append({
+             spreadsheetId: spreadsheetId,
+             range: 'Sheet1',
+             valueInputOption: 'RAW',
+             insertDataOption: 'INSERT_ROWS',
+             requestBody: resource
+         })
+     
+         res.json({ "success": true, "data": data.data, "message": null });
+     } catch (err: any) {
+         Logging.error(err.message);
+         return res.status(404).json({ "success": false, "data": null, "message": "Error in writing data to the sheets" });
+     }
+     * 
+     */
     
-        oauth2Client.refreshAccessToken((err, tokens: any) => {
-            if (err) return console.error(err)
-    
-            oauth2Client.setCredentials({
-                access_token: tokens.access_token
-            })
-        });
-    
-        const doc = google.sheets({ version: "v4", auth: oauth2Client });
-        
-        const resource = {
-            values: [
-                [ responseid, name, yearOfJoining, phone, email, userId ]
-            ]
-        }
-    
-        let data = await doc.spreadsheets.values.append({
-            spreadsheetId: spreadsheetId,
-            range: 'Sheet1',
-            valueInputOption: 'RAW',
-            insertDataOption: 'INSERT_ROWS',
-            requestBody: resource
-        })
-    
-        res.json({ "success": true, "data": data.data, "message": null });
-    } catch (err: any) {
-        Logging.error(err.message);
-        return res.status(404).json({ "success": false, "data": null, "message": "Error in writing data to the sheets" });
-    }
+    await sheetProducer.publishMessage(config.queue.exchangeName, config.queue.queueName, req.body);
+
+    return res.sendStatus(200);
 }
 
 export { createSpreadSheetController, appendSpreadSheetController };
